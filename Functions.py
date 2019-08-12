41ef05efe3cea0098cfa3a55d806945e14f62177
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-08-12 18:27:50 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,21 @@
+def mukki(fname):
+    print(fname + " singh ")
+
+mukki("mukks")
+mukki("mukul")
+mukki("micky")
+
+def mukki2(country = "india"):
+  print("i am from " + country)
+
+mukki2("america")
+mukki2()
+mukki2()
+
+def mukki3(fruits):
+    print("it is nice")
+    for i in fruits:
+      print(i)
+food=["patoto","tamato"]
+
+mukki3(food)
