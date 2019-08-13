b756f0813a6123dd0027960fa258f9627d6866ee
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-08-13 19:26:19 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,15 @@
+num1=input("enter first number:")
+print("\n")
+num2=input("enter second number:")
+print("\n")
+print("values of num1 before swapping:",num1)
+print("values of num2 before swapping:",num2)
+
+
+temp=num1
+num1=num2
+num2=temp
+
+
+print("values of num1 after swapping:",num1)
+print("values of num2 after swapping:",num2)
