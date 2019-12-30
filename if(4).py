5a0712eca7e0bc750715aab57c66cc4342f02b7a
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2019-12-30 19:25:26 +0530

    Add files via upload

---
+++ if(4).py
@@ -0,0 +1,124 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("greatest number:")
+greatest number:
+>>> a = 10
+>>> b = 12
+>>> c = 13
+>>> if
+SyntaxError: invalid syntax
+>>> if a < b:
+	print("value")
+
+	
+value
+>>> if b > c:
+	print("big")
+
+	
+>>> kjkjf
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    kjkjf
+NameError: name 'kjkjf' is not defined
+>>> knfkewf
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    knfkewf
+NameError: name 'knfkewf' is not defined
+>>> waterlevel = 100
+>>> waterlevel
+100
+>>> x = waterlevel
+>>> x
+100
+>>> if x > 50
+SyntaxError: invalid syntax
+>>> if x > 40:
+	x == 50
+	printf(x)
+ elif: x > 20 and x <= 100
+ 
+SyntaxError: unindent does not match any outer indentation level
+>>>  if x > 40:
+	x == 50
+	printf(x)
+ elif x > 20 and x <= 100:
+	 
+SyntaxError: unexpected indent
+>>>  if x > 40:
+	x == 50
+	printf(x)
+     elif x > 20 and x <= 100:
+	     
+SyntaxError: unexpected indent
+>>> if x > 40:
+	x == 50
+	printf(x)
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+	
+False
+Traceback (most recent call last):
+  File "<pyshell#28>", line 3, in <module>
+    printf(x)
+NameError: name 'printf' is not defined
+>>> x
+100
+>>> if x > 40:
+	x == 100
+	printf(x)
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+	
+True
+Traceback (most recent call last):
+  File "<pyshell#31>", line 3, in <module>
+    printf(x)
+NameError: name 'printf' is not defined
+>>> if x > 40:
+	print("oye")
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+	
+oye
+>>> x = 60
+>>> x
+60
+>>> if x > 40:
+	print("oye")
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+oye
+>>> if x == 40:
+	print("oye")
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+	
+everytime
+>>> x = 18
+>>> if x == 40:
+	print("oye")
+elif x > 20 and x <= 100:
+	print("everytime")
+elif x > 12 or x <=100:
+	print("indent")
+
+	
+indent
+>>> 
