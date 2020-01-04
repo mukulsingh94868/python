5631b0ae940388406347a1068ee10eaf330366ba
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-01-04 20:41:05 +0530

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,16 @@
+for i in range(5):
+	print("hello world")
+
+pressure = 10
+ppressure = 20
+
+while pressure <= ppressure:
+	pressure = pressure + 1
+	
+
+	print(pressure)
+number = 3
+number1 = 4
+while number < number1:
+	number = number + number1
+	print(number)
