78114c3648af7cff0fc1e9df9f6527fa95916aa0
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-01-04 19:20:37 +0530

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,29 @@
+number = ['cat','dog','lion','ant']
+print(number)
+for i in number:
+    i = i +"  animal"
+   
+    print(i)
+    print(len(i))
+number1 = "auatic"    
+  
+numbers = ['fish','shark','jellyfish','starfish']    
+print(numbers)    
+for w in numbers:
+    w = w +"s"
+    
+    print(w)
+    print(len(w))
+number2 = " mammals"    
+numberlist = [1,2,3,4,5]
+for z in numberlist:
+	z = z + 2
+	print(z)
+
+
+   
+mukki = number + numbers
+print(mukki)
+mukki2 = number1 + number2
+print(mukki2)
+print("done")
