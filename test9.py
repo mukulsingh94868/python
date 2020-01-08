01f4db78b211e44a3f397e18aa336604610de67f
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-01-08 20:48:07 +0530

    Add files via upload

---
+++ test9.py
@@ -0,0 +1,23 @@
+
+def printFunction():
+
+  print("this is my print function")
+
+
+  print("fuck u")
+
+  variable = [1,2,3,4,5]
+  for i in variable:
+    i = i + 1
+    print(i)  
+printFunction()
+print("now addition is")
+
+def mukki():
+	x = 1
+	y = 2
+	z = x + y
+	return z
+
+value = mukki()	
+print(value)
