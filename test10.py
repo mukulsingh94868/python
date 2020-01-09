fd8805b3e06e1b38168c54ef950224110669b2f1
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-01-09 20:03:15 +0530

    Add files via upload

---
+++ test10.py
@@ -0,0 +1,34 @@
+number1 = 12
+number2 = 13
+
+def mukki(num1,num2):
+	
+	
+	num3 = number1 + number2
+	return num3
+def mukki1(num1,num2):	
+
+	num5 = number1 - number1
+	return num5
+def mukki2(num1,num2):	
+
+	num6 = number1 * number2
+	return num6
+def mukki3(num1,num2):	
+	num7 = number1 / number2
+	return num7
+def mukki4(num1,num2):	
+	num8 = number1 % number2
+	return num8
+for i in range(10):
+	print("fuck off")
+num4 = mukki(number1,number2)
+print(num4)
+yups = mukki1(number1,number2)
+print(yups)
+yups2 = mukki2(number1,number2)
+print(yups2)
+yups3 = mukki3(number1,number2)
+print(yups3)
+yups4 = mukki4(number1,number2)
+print(yups4)
