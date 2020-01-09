87029caf74e62c3e2b62beb6681f0ee5806ecf42
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-01-09 20:04:05 +0530

    Add files via upload

---
+++ yes2.py
@@ -0,0 +1,36 @@
+number1 = input("enter the number:")
+print(number1)
+number2 = input("enter the second number:")
+print(number2)
+
+def mukki(num1,num2):
+	
+	
+	num3 = number1 + number2
+	return num3
+def mukki1(num1,num2):	
+
+	num5 = number1 - number2
+	return num5
+def mukki2(num1,num2):	
+
+	num6 = number1 * number2
+	return num6
+def mukki3(num1,num2):	
+	num7 = number1 / number2
+	return num7
+def mukki4(num1,num2):	
+	num8 = number1 % number2
+	return num8
+for i in range(10):
+	print("fuck off")
+num4 = mukki(number1,number2)
+print(num4)
+yups = mukki1(number1,number2)
+print(yups)
+yups2 = mukki2(number1,number2)
+print(yups2)
+yups3 = mukki3(number1,number2)
+print(yups3)
+yups4 = mukki4(number1,number2)
+print(yups4)
