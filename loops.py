e70264d958f1762225acf87d246fa798713ae4b0
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-05-15 14:36:28 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,26 @@
+x = int(input("enter one number:"))
+y = int(input("enter second number:"))
+
+if(x < y):
+	for i in range(10):
+		print("hello world")
+
+else:
+	for i in range(5):
+		print("ohh yeah")
+
+
+
+z = str(input("give string:"))
+for i in z:
+	print("mukki")
+
+
+
+a = [0,1,2,3,4,5,6,7,8,9]
+print(a)
+
+for i in a:
+	a[i] = input("enter your name:")
+
+print(a)
