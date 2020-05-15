a003b9d5b051fca1c0ed16ee213d8f2efe54a1c4
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-05-15 14:39:24 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,17 @@
+x = 3
+y = 3
+z = 4
+
+
+if(x == y):
+	if(x != z):
+		if(y == z):
+
+			
+			if(z > x):
+				print("x is equal to y")
+        else:
+            print("yes")     
+       
+        
+
