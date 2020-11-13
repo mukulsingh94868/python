09e64e0c9abedcd10ee62887fd2328ce879efda0
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-11-13 12:20:49 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fibbonacci(n):
+	x = 0
+	y = 1
+	z = 0
+	while(z<=n):
+		print(z)
+		x = y
+		y = z
+		z = x + y
+print("fibbonacci series")
+
+number = fibbonacci(5)
+print(number)
+
+    	
+    
