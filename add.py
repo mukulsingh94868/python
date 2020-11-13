7b6b10c5ae9f97ae5b3f67b97100abc69dea6c83
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-11-13 12:21:31 +0530

    Add files via upload

--- add.py
+++ add.py
@@ -1,45 +1,8 @@
-def add(x, y):
-    return x + y
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
 
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
+sum = float(num1) + float(num2)
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # Take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # Check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        break
-    else:
-        print("Invalid Input")
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
